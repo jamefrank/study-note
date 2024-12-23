11299ebd0eff143ca5ed87226935675b27676d3e
frank <13121950875@163.com>
2024-12-24 00:32:13 +0800

    feat (tdx): pull qfq data by tonghushun

---
+++ study_note/tdx/read_fq_data.py
@@ -0,0 +1,37 @@
+'''
+Author: frank 13121950875@163.com
+Date: 2024-12-24 00:12:37
+LastEditors: frank 13121950875@163.com
+LastEditTime: 2024-12-24 00:37:15
+FilePath: /study-note/study_note/tdx/read_fq_data.py
+Description: 
+
+读取复权数据
+"00": 不复权, "01": 前复权, "02": 后复权
+
+Copyright (c) 2024 by Frank, All Rights Reserved. 
+'''
+
+
+from mootdx.contrib.adjust import get_adjust_year
+
+
+NFQ = "00"
+QFQ = "01"
+HFQ = "02"
+
+# data = get_adjust_year(symbol='000001', year='2021', factor='01')
+# print(data)
+
+
+from mootdx.quotes import Quotes
+
+client = Quotes.factory(market='std')
+client.bars(symbol='600036', frequency=9, offset=10)
+
+# 前复权
+data = client.bars(symbol='600036', adjust='qfq')
+print(data)
+# 后复权
+data  = client.bars(symbol='600036', adjust='hfq')
+print(data)
